998b291e1688f1be99091f10bba262ad823eaf2e
chaimvaid <chaim@daneltech.com>
2019-10-02 20:57:07 +0300

    lesson end

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const tasksRouter = require('./app/api/tasks_router').tasksRouter;
 
 app.use('/tasks', tasksRouter)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app
+
 
--- app/api/tasks_router.js
+++ app/api/tasks_router.js
@@ -4,7 +4,7 @@ var tasksRouter = express.Router()
 
 // define the home page route
 tasksRouter.get('/', function (req, res) {
-  res.send('Birds home page')
+  res.send('all tasks')
 })
 // define the about route
 tasksRouter.get('/about', function (req, res) {
