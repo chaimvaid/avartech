6905b7184a05a6e720a85a8c3f5bcdefef03d17a
chaimvaid <chaim@daneltech.com>
2019-11-07 09:06:55 +0200

    lesson 6 ended

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,14 @@
 const request = require('supertest');
 const app = require('../app')
+const db = require('../models')
+const path = require('path')
+var Umzug = require("umzug");
+require('mysql2/node_modules/iconv-lite').encodingExists('foo');
+
+
 
 describe('Test the tasksRouter root path', () => {
-    test('It should response the GET method', () => {
+    it('should response the GET method', () => {
         return request(app).get("/tasks").then(response => {
             // console.log(response)
             expect(response.statusCode).toBe(200)
@@ -12,7 +18,33 @@ describe('Test the tasksRouter root path', () => {
 })
 
 describe('Test the create user path', () => {
-    test('It should response the POST method', () => {
+    beforeEach(function(done) {
+        // Setup the database
+      //   db.sequelize
+      //     .getQueryInterface()
+      //     .dropAllTables()
+      //     .complete(function() { done(); }, function(err) { done(err); });
+      // });
+        const umzug = new Umzug({
+        migrations: {
+        params: [ db.sequelize.getQueryInterface(), db.Sequelize ],
+        path: "migrations"
+        },
+        storage: "sequelize",
+        storageOptions: {
+        sequelize: db.sequelize
+        },
+        logging: console.log
+        });
+
+        umzug.execute({
+        migrations: ["20191023191008-create-user"],
+        method: "up"
+        }).then(function(migrations) {
+            console.log(migrations);
+        });
+    })
+    it('should response the POST method', () => {
         return request(app).post("/users").then(response => {
             expect(response.statusCode).toBe(200)
         })
